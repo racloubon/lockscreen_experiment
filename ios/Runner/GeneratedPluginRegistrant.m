9925409014cf68b33dbcbe228f06def214d57cd8
Rachel Bonny <rachel.bonny@koahealth.com>
2021-09-08 12:06:41 +0200

    feat: app

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_lockscreen_check/FlutterLockscreenCheckPlugin.h>)
+#import <flutter_lockscreen_check/FlutterLockscreenCheckPlugin.h>
+#else
+@import flutter_lockscreen_check;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterLockscreenCheckPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterLockscreenCheckPlugin"]];
+}
+
+@end
